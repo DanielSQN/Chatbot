f888812293279c2cd76175098331cb21b387dbc4
DanielSQN <87499720+DanielSQN@users.noreply.github.com>
2022-10-20 16:28:43 -0500

    Carga nueva version 20-Oct
    Se agrega logica de recomendacion almacenamiento y opcion de respuesta

--- app/views.py
+++ app/views.py
@@ -5,8 +5,22 @@ import random
 import time
 
 def get_response(user_input):
-    split_message = re.split(r'\s|[,:;.?!-_]\s*', user_input.lower())
+    # validate if user input is a string
+    # if not isinstance(user_input, str):
+    #     print('Error: user input is not a string')
+    # else:
+    #     print('User input: ' + user_input)
+
+    try:
+        print('User input: ' + user_input)
+    except:
+        print('Error: user input is not a string')
+
+    # split_message = re.split(r'\s|[,:;.?!-_]\s*', user_input.lower())
+    split_message = re.split(r'\s|[ ]\s*', user_input.lower())
+    print('Split message: ' + str(split_message))
     response = check_all_messages(split_message)
+    print('Bot response: ' + response)
     return response
 
 def message_probability(user_message, recognized_words, single_response=False, required_word=[]):
@@ -36,8 +50,24 @@ def check_all_messages(message):
             highest_prob[bot_response] = message_probability(message, list_of_words, single_response, required_words)
 
         #response('... respuesta ...', ['test'], required_words=['test'])
-        response('Hola, bienvenid@ a nuestro chat ... ¿En que podemos ayudarte hoy?', ['hola', 'saludos', 'buenas'], single_response = True)
-        response('Te podemos ayudar en lo que necesitas! actualmente tenemos integracion con (Microsoft Azure, Oracle, AWS)', ['informacion', 'cloud', 'migracion', 'consultoria'], single_response=True)
+        response('Hola, bienvenid@ a nuestro chat ... ¿En que podemos ayudarte hoy?', ['hola', 'saludos', 'buenas', 'hi'], single_response = True)
+        response('Te podemos ayudar en lo que necesitas! actualmente tenemos integracion con (Microsoft Azure, Oracle, AWS)', ['informacion', 'información', 'nube','cloud', 'migracion', 'migración', 'consultoria', 'asesoria', 'asesoramiento'], single_response=True)
+        response('Gracias a ti por visitar nuestro chat, hasta luego!', ['gracias', 'te lo agradezco', 'thanks'], single_response=True)
+        
+        response('Ofrecemos servicios para Maquinas virtuales, Almacenamiento y Bases de datos', ['buscando', 'cuales', 'opciones', 'servicios'], single_response=True)
+
+        # almacenamiento
+        response('¡Muy bien! ¿Cuánto almacenamiento vas a necesitar? 1) 100GB - 2) 250GB', ['almacenamiento'], single_response=True)
+
+        response('Requieres 100GB de almacenamiento', [100, '100', '100 GB', '100GB'], single_response=True)
+        response('Requieres 250GB de almacenamiento', [250, '250', '250 GB', '250GB'], single_response=True)
+
+        # maquinas virtuales
+        response('¡Que bien! Ahora podrias darme los parametros de tu VM, comenzemos por la RAM', ['maquina virtual', 'VM', 'maquina', 'virtual'], single_response=True)
+        response('Para tu VM prefieres elegir el numero de nucleos en tu procesador o el numero de CPU en tu maquina?', ['nuclueo', 'CPU'], single_response=True)
+
+        response('Super!', ['si', 'yes'], single_response=True)
+
         response('Gracias a ti por visitar nuestro chat, hasta luego!', ['gracias', 'te lo agradezco', 'thanks'], single_response=True)
 
         best_match = max(highest_prob, key=highest_prob.get)
@@ -60,7 +90,7 @@ def json_example():
     contId = request_data['contId'] 
     user_input = request_data['user_input'] 
 
-    time.sleep(3)
+    time.sleep(1.5)
     if int(contId) == 1:
         return 'Nombre|' + user_input
     return get_response(user_input)
@@ -87,7 +117,7 @@ def getjson_example():
 # def home():
 #     return "Hello world!"
 
-# @app.route('/template')
+# @app.route('/template') 
 @app.route('/')
 def template():
     return render_template('home.html')
